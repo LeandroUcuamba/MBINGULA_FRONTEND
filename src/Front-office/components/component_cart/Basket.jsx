1ae73aa4f55f99e15094c77102cfb3b667d7c75e
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2024-10-20 15:35:42 +0100

    Alter

--- src/Front-office/components/component_cart/Basket.jsx
+++ src/Front-office/components/component_cart/Basket.jsx
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 import logoImg from '../../assets/logo.png';
 import carimboImg from '../../assets/carimbo.png';
+
+import axios from 'axios';
 
 export default function Basket(props) {
   const { cartItems, onAdd, onRemove } = props;
@@ -10,6 +12,16 @@ export default function Basket(props) {
   const totalPrice = itemsPrice;
 
   const basketRef = useRef(null);
+
+  const [formData, setFormData] = useState({
+    tipoConsumo: '',
+    valorTotal: totalPrice,
+    itemsPedido: '',
+    metodoPagamento: '',
+    numeroMesa: '',
+    userName: '',
+    userPhone: ''
+  });
 
   const handleDownloadPDF = () => {
     const input = basketRef.current;
@@ -40,7 +52,6 @@ export default function Basket(props) {
   
     Promise.all([getBase64Image(logoImg), getBase64Image(carimboImg)])
       .then(([logoData, carimboData]) => {
-        
         const logoWidth = 100;
         const logoHeight = 18;
         pdf.addImage(logoData, 'PNG', 6, 7, logoWidth, logoHeight);
@@ -80,6 +91,44 @@ export default function Basket(props) {
         console.error('Erro ao carregar as imagens:', error);
       });
   };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    const updatedItemsPedido = cartItems.map(item => `${item.qty} x ${item.name}`).join(', ');
+  
+    const payload = {
+      ...formData,
+      itemsPedido: updatedItemsPedido,
+      valorTotal: totalPrice,
+      numeroMesa: parseInt(formData.numeroMesa, 10),
+    };
+  
+    console.log("Dados enviados para o backend:", payload);
+  
+    try {
+      const response = await axios.post('http://localhost:3000/create-pedidoLocal', payload);
+      console.log("Resposta do servidor:", response);
+      if (response.status === 200) {
+        alert('Pedido enviado com sucesso!');
+      } else {
+        console.error('Erro na resposta do servidor:', response);
+        alert('Erro ao enviar o pedido.');
+      }
+    } catch (error) {
+      console.error('Erro ao enviar o pedido:', error.response || error.message);
+      alert('Erro ao enviar o pedido.');
+    }
+  };
+  
 
   return (
     <aside className="block col-2">
@@ -123,11 +172,71 @@ export default function Basket(props) {
       </div>
 
       {cartItems.length !== 0 && (
-        <div className="row">
-          <button onClick={handleDownloadPDF} id="btn-solicitar">
-            Solicitar Pedido
-          </button>
-        </div>
+        <>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>
+                Tipo de Consumo:
+                <input 
+                  type="text" 
+                  name="tipoConsumo" 
+                  value={formData.tipoConsumo} 
+                  onChange={handleChange} 
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Número da Mesa:
+                <input 
+                  type="number" 
+                  name="numeroMesa" 
+                  value={formData.numeroMesa} 
+                  onChange={handleChange} 
+                  required
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Nome:
+                <input 
+                  type="text" 
+                  name="userName" 
+                  value={formData.userName} 
+                  onChange={handleChange} 
+                  required 
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Telefone:
+                <input 
+                  type="tel" 
+                  name="userPhone" 
+                  value={formData.userPhone} 
+                  onChange={handleChange} 
+                  required 
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Método de Pagamento:
+                <input 
+                  type="text" 
+                  name="metodoPagamento" 
+                  value={formData.metodoPagamento} 
+                  onChange={handleChange} 
+                />
+              </label>
+            </div>
+            <div>
+              <button type="submit" onClick={handleDownloadPDF}>Enviar Pedido</button>
+            </div>
+          </form>
+        </>
       )}
     </aside>
   );
