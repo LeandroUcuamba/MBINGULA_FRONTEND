657789923fde111fa757855c6eec783654dce8db
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2024-10-21 22:16:24 +0100

    add and change icons

--- src/Front-office/components/Menu.jsx
+++ src/Front-office/components/Menu.jsx
@@ -4,6 +4,12 @@ import { NavLink, Link } from "react-router-dom";
 import { RiRestaurant2Fill } from "react-icons/ri";
 import { IoHome } from "react-icons/io5";
 import { AiOutlineQuestionCircle, AiOutlinePlusCircle, AiOutlineUser, AiOutlineReload } from "react-icons/ai";
+import { AiOutlineFileDone } from "react-icons/ai";
+import { AiFillSun } from "react-icons/ai";
+import { AiFillTool } from "react-icons/ai";
+import { AiOutlineContainer } from "react-icons/ai";
+import { AiOutlineEdit } from "react-icons/ai";
+import { AiOutlineMail } from "react-icons/ai";
 import { BiLogOut, BiLogIn } from "react-icons/bi";
 
 import { AuthContext } from '../../context/AuthContext';
@@ -56,25 +62,25 @@ function Navbar() {
                   )}
                 </li>
                 <li><NavLink to="/Back-office/App_BackOffice"><AiOutlineUser className="icones-menu" /> Admin</NavLink></li>
-                <li><NavLink to="/Front-office/pages/mesaReservada"> Reservar Mesa</NavLink></li>
+                <li><NavLink to="/Front-office/pages/mesaReservada"><AiOutlineFileDone className="icones-menu" /> Reservar Mesa</NavLink></li>
                 <li>
                   <NavLink to="#" onClick={handleShowMore}><AiOutlinePlusCircle className="icones-menu" /> Ver mais</NavLink>
                 </li>
               </>
             ) : showOrderMenu ? (
               <>
-                <li><NavLink to="/Front-office/pages/PedidoLocal"><IoHome className="icones-menu" /> Pedido no local</NavLink></li>
-                <li><NavLink to="/"><IoHome className="icones-menu" /> Pedido de Casa</NavLink></li>
+                <li><NavLink to="/Front-office/pages/PedidoLocal"><AiOutlineMail className="icones-menu" /> Pedido no local</NavLink></li>
+                <li><NavLink to="/"><AiOutlineMail className="icones-menu" /> Pedido de Casa</NavLink></li>
                 <li><NavLink to="#" onClick={handleBack}><AiOutlineReload className="icones-menu" /> Voltar</NavLink></li>
               </>
             ) : (
               <>
-                <li><NavLink to="/Front-office/pages/AtividadeCasa"><IoHome className="icones-menu" /> Atividades</NavLink></li>
-                <li><NavLink to="/Front-office/pages/CardapioItems"><IoHome className="icones-menu" /> Cardápio </NavLink></li>
-                <li><NavLink to="/Front-office/pages/Servicos"><IoHome className="icones-menu" /> Serviços</NavLink></li>
-                <li><NavLink to="/Front-office/pages/pages/avaliacao/create"><IoHome className="icones-menu" /> Avaliação</NavLink></li>
+                <li><NavLink to="/Front-office/pages/AtividadeCasa"><AiFillSun className="icones-menu" /> Atividades</NavLink></li>
+                <li><NavLink to="/Front-office/pages/CardapioItems"><AiOutlineContainer className="icones-menu" /> Cardápio </NavLink></li>
+                <li><NavLink to="/Front-office/pages/Servicos"><AiFillTool className="icones-menu" /> Serviços</NavLink></li>
+                <li><NavLink to="/Front-office/pages/pages/avaliacao/create"><AiOutlineEdit className="icones-menu" /> Avaliação</NavLink></li>
                 <li>
-                  <NavLink to="#" onClick={handleOrderClick}><IoHome className="icones-menu" /> Pedido</NavLink>
+                  <NavLink to="#" onClick={handleOrderClick}><AiOutlineMail className="icones-menu" /> Pedido</NavLink>
                 </li>
                 <li><NavLink to="#" onClick={handleBack}><AiOutlineReload className="icones-menu" /> Voltar</NavLink></li>
               </>
