021842672313c2706a861f7bd7ee3d6347c81e66
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2024-11-03 22:17:43 +0100

    Add interceptor axios error

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,19 @@
 import axios from 'axios';
 
-export const api = new axios.create({
-    baseURL: 'http://localhost:3000',
+export const api = axios.create({
+  baseURL: 'http://localhost:3000',
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.data) {
+      alert(error.response.data.message || "Ocorreu um erro. Verifique as informações e tente novamente.");
+    } else {
+      alert("Erro de conexão com o servidor.");
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
